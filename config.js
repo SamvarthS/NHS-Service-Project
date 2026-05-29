48d5e884d33ecf60922a637dd55f7c0f0e27bebc
Samvarth Seegehalli <ContactSamvarth@gmail.com>
2026-05-29 18:30:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = window.APP_CONFIG || {
-  API_BASE_URL: "https://nhs-service-project-rom79ws3p-samvarths-projects-c9e04ebf.vercel.app"
+  API_BASE_URL: "https://nhs-service-project.vercel.app"
 };
